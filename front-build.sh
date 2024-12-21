921b685a510f833437d9fee3b4a2aa033d3aab55
orgic <2011939212@qq.com>
2024-12-22 00:12:28 +0800

    修复编译脚本

--- front-build.sh
+++ front-build.sh
@@ -12,13 +12,9 @@ do
     echo "开始编译${i}项目"
     npm i
     npx vite build
-    if [ $i == "workbench" ]; then
-        cp -r $1/$i/dist/* $2/
-    else
-        cp -r dist $2/$i
-    fi
 done
 echo "开始编译markdown项目"
 cd $1/markdown
+npm i
 node --max-old-space-size=2048 ./node_modules/.bin/vite build
 cp -r $1/markdown/dist $2/markdown
