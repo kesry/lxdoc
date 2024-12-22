7fad2d151faaa71d7fa623f28be80d8347f4ba73
orgic <2011939212@qq.com>
2024-12-22 14:51:02 +0800

    修正Dockerfile

--- init.sql
+++ init.sql
@@ -1,23 +1,23 @@
 CREATE TABLE IF NOT EXISTS sys_user_info
 (
     id        integer PRIMARY KEY AUTOINCREMENT,
-    user_name varchar(64)           ,
+    user_name varchar(64)           DEFAULT NULL,
     account   varchar(32)  NOT NULL ,
     password  varchar(256) NOT NULL,
-    avatar    varchar(1024)         ,
+    avatar    varchar(1024)         DEFAULT NULL,
     create_at DATETIME     NOT NULL ,
-    version   int NOT NULL DEFAULT 0,
+    version   int NOT NULL DEFAULT ,
     update_at DATETIME     NOT NULL ,
-    status    int          NOT NULL DEFAULT 0
+    status    int          NOT NULL DEFAULT 
 );
 
 CREATE TABLE IF NOT EXISTS sys_attachment
 (
     id         integer PRIMARY KEY AUTOINCREMENT,
-    name       varchar(256)          ,
+    name       varchar(256)          DEFAULT NULL,
     path       varchar(512) NOT NULL,
-    remark     varchar(512)          ,
-    size       integer NOT NULL DEFAULT 0,
+    remark     varchar(512)          DEFAULT NULL,
+    size       integer NOT NULL DEFAULT ,
     version    int NOT NULL DEFAULT 0,
     creator_id integer NOT NULL ,
     create_at  DATETIME     NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS sys_user_config
 (
     id             integer PRIMARY KEY AUTOINCREMENT,
     config_type    varchar(32) NOT NULL ,
-    config_content varchar(1024)         ,
+    config_content varchar(1024)        DEFAULT NULL ,
     version        int NOT NULL DEFAULT 0,
     user_id        integer NOT NULL ,
     create_at      DATETIME    NOT NULL ,
@@ -40,14 +40,14 @@ CREATE TABLE IF NOT EXISTS sys_user_config
 CREATE TABLE IF NOT EXISTS doc_file_folder
 (
     id           integer PRIMARY KEY AUTOINCREMENT,
-    parent_id   integer NOT NULl
+    parent_id   integer NOT NULl,
     name         varchar(128) NOT NULL,
     file_count   int NOT NULL DEFAULT 0 ,
     folder_count int NOT NULL DEFAULT 0,
     format       int NOT NULL ,
     file_type    varchar(32) NOT NULL,
     collected    int NOT NULL,
-    img          varchar(1024) ,
+    img          varchar(1024) DEFAULT NULL,
     version      int NOT NULL DEFAULT 0 ,
     creator_id   integer NOT NULL ,
     create_at    datetime    NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS doc_collect_folder
     name      varchar(128) NOT NULL,
     user_id   integer NOT NULL,
     create_at DATETIME    NOT NULL,
-    status    int         NOT NULL DEFAULT 0
+    status    int         NOT NULL DEFAULT 
 );
 
 CREATE TABLE IF NOT EXISTS doc_recycle
